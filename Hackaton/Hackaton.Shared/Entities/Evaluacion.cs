dcd03bc7ff9a5e9b0eb8e2f08ce517cafce1aa98
ivanmorales <ivanandres010605@gmail.com>
2024-10-04 08:11:35 -0500

    correcion de clase y correcion de referencias

--- Hackaton/Hackaton.Shared/Entities/Evaluacion.cs
+++ Hackaton/Hackaton.Shared/Entities/Evaluacion.cs
@@ -8,25 +8,26 @@ using System.Threading.Tasks;
 namespace Hackaton.Shared.Entities
 {
     public class Evaluacion
+    {
         public int Id { get; set; }
 
-    [Required]
-    public int PuntuacionInnovacion { get; set; }
+        [Required]
+        public int PuntuacionInnovacion { get; set; }
 
-    [Required]
-    public int PuntuacionFuncionalidad { get; set; }
+        [Required]
+        public int PuntuacionFuncionalidad { get; set; }
 
-    [Required]
-    public int PuntuacionPresentacion { get; set; }
+        [Required]
+        public int PuntuacionPresentacion { get; set; }
 
-    [MaxLength(500, ErrorMessage = "Los comentarios no pueden exceder los 500 caracteres")]
-    public string? Comentarios { get; set; }
+        [MaxLength(500, ErrorMessage = "Los comentarios no pueden exceder los 500 caracteres")]
+        public string? Comentarios { get; set; }
 
-    public int ProyectoId { get; set; } // Clave foránea para proyecto
-    public Proyecto Proyecto { get; set; } // Relación con Proyecto
+        public int ProyectoId { get; set; } // Clave foránea para proyecto
 
-    public int MentorId { get; set; } // Clave foránea para mentor
-    public Mentor Mentor { get; set; } // Relación con Mentor
-    {
+        public required Proyecto Proyecto { get; set; }  // Modificador required para C# 11+
+
+        public int MentorId { get; set; } // Clave foránea para mentor
+        public Mentor? Mentor { get; set; } // Relación con Mentor
     }
 }
